e9bbb32e46373f5b6d6e483835c8f05f508e0389
josephsmendoza <josephsmendoza@gmail.com>
2020-08-04 02:34:00 +0000

    update readme

--- src/Install.hx
+++ src/Install.hx
@@ -11,6 +11,7 @@ class Install {
         paths.add("haxelib.json");
 		paths.add("extraParams.hxml");
 		paths.add("LICENSE");
+		paths.add("README.md");
         final entries = new List<Entry>();
 		while (paths.length > 0) {
 			final path = paths.pop();
